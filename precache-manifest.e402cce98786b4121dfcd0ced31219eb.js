e9c9f3da4a0d4d26c29def2d75f9bb0d0e29b770
Alina <alina90565@gmail.com>
2020-10-24 21:26:46 +0300

    Updates

---
+++ precache-manifest.e402cce98786b4121dfcd0ced31219eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9247bed54b04b6ab9e4d3b5151d5a683",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f50d79591266c3d3b58c",
+    "url": "./static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "75d504c4a2030215119f",
+    "url": "./static/css/main.c5c09bf3.chunk.css"
+  },
+  {
+    "revision": "f50d79591266c3d3b58c",
+    "url": "./static/js/2.5027c62e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.5027c62e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75d504c4a2030215119f",
+    "url": "./static/js/main.22d303d4.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
